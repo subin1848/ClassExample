fef852b50a0b9b5a11d61c5cf3122d7c55e1ae8d
subin1848 <s2315@e-mirim.hs.kr>
2025-06-13 10:42:42 +0900

    상속 관계에서의 생성자 호출 예

--- Program.cs
+++ Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace ClassExample
 {
-    class Program
+    internal class Program
     {
+        class Parent
+        {
+            public Parent() { Console.WriteLine("부모 생성자"); }
+            public Parent(string name) { Console.WriteLine("부모 생성자"); }
+
+            public Parent(float param) { Console.WriteLine("Parent(double param)"); }
+        }
+        class Child : Parent
+        {
+            public Child() : base("child")
+            {
+                Console.WriteLine("Child()");
+            }
+            public Child(string name) : base(name)
+            {
+                Console.WriteLine("Child(name)");
+            }
+            public Child(float param) : base(param)
+            {
+                Console.WriteLine("Child(dobule param)");
+            }
+        }
         static void Main(string[] args)
         {
+            Child childA = new Child("abc");
+            Child childB = new Child(3L);
+            // int < long < float < double 
+            //  4     8      4      8
         }
     }
 }
